074b0f3d8c0290ed12cbd0cd663a1863936d23a0
Rhyza Velasco <rhyza.velasco@gmail.com>
2018-03-08 16:57:50 -0500

    Create static ContactRow component

--- contacts-app/src/index.js
+++ contacts-app/src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+class ContactRow extends React.Component {
+    render() {
+        const contact = this.props.contact;
 
-
+        return (
+            <tr className="contact-row">
+                <td>{contact.firstName}</td>
+                <td>{contact.lastName}</td>
+                <td>{contact.country}</td>
+                <td>{contact.street}</td>
+                <td>{contact.city}</td>
+                <td>{contact.state}</td>
+                <td>{contact.zip}</td>
+                <td>{contact.phone}</td>
+            </tr>
+        );
+    }
+}
 
 //==============================================================================
 
