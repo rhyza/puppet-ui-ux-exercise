22e2a0a4a55abbe03f26fc1e44e05d2ac20dda88
Rhyza Velasco <rhyza.velasco@gmail.com>
2018-03-09 02:29:37 -0500

    Refactor
    Remove unnecessary variable

--- contacts-app/src/index.js
+++ contacts-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 const CONTACTS = [
   {firstName:"Zachary",lastName:"Borgetti",country:"USA",street:"2234 3rd Ave Site 3",city:"Seattle",state:"WA",zip:"98101",phone:"206-778-5741"},
+  {firstName:"Zach",lastName:"Borgetti",country:"USA",street:"2234 3rd Ave Site 3",city:"Seattle",state:"WA",zip:"98101",phone:"206-778-5741"},
   {firstName:"Landon",lastName:"Donovan",country:"USA",street:"1800 S Avalon Blvd",city:"Carson",state:"CA",zip:"90746",phone:"310-630-2200"},
   {firstName:"Samuel L.",lastName:"Jackson",country:"USA",street:"2226 2nd Ave",city:"Seattle",state:"WA",zip:"98121",phone:"206-441-5660"},
   {firstName:"Micheal",lastName:"Jordan",country:"USA",street:"1901 West Madison Street",city:"Chicago",state:"IL",zip:"60612",phone:"312-455-4000"},
@@ -53,11 +54,9 @@ class ContactTable extends React.Component {
   }
 
   render() {
-    const numContacts = parseInt(this.props.numContacts, 10);
-    const multiplierBase = this.props.page * this.props.numItems;
-    const contactStart =  parseInt(multiplierBase, 10);
-    const contactEnd = parseInt(
-        Math.min(multiplierBase + this.props.numItems, numContacts), 10);
+    const numContacts = this.props.contacts.length;
+    const contactStart =  this.props.page * this.props.numItems;
+    const contactEnd = Math.min(contactStart + this.props.numItems, numContacts);
 
     const contacts = this.sortBy(this.props.contacts, this.props.sort);
     const currentContacts = contacts.slice(contactStart, contactEnd);
